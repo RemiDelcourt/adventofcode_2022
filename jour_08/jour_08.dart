dfacf7f339faaf5e057eef5ecba9033cebaa9c26
Rémi Delcourt <fralacticus@gmail.com>
2022-12-08 19:23:16 +0100

    jour_08 terminé

--- jour_08/jour_08.dart
+++ jour_08/jour_08.dart
@@ -1,5 +1,6 @@
 import "dart:io";
-import 'dart:math';
+
+import 'package:collection/collection.dart';
 
 const String test = """
 30373
@@ -9,7 +10,6 @@ const String test = """
 35390
 """;
 
-List<String> reponse = ["00","01","02","03","04","10","11","12","14","20","21","23","24","30","32","34","40","41","42","43","44"];
 void main(){
   List<String> fichier = File("./jour_08/input.txt").readAsLinesSync();
   //fichier = test.split("\n")..removeLast();
@@ -23,6 +23,7 @@ void main(){
   int nbColonne = carte.first.length;
 
 
+  /* Partie 1 */
   List<String> listePos = [];
   for(int ligne = 0; ligne < nbLigne; ligne++){
     // De gauche à droite
@@ -46,8 +47,6 @@ void main(){
     }
   }
 
-
-
   for(int colonne = 0; colonne < nbColonne; colonne++){
     // De haut en bas
     listePos.add("L${0}C${colonne}");
@@ -70,11 +69,70 @@ void main(){
     }
   }
 
-  // nb arbres brut
-  print(listePos);
-  // nb arbres visibles
-  print(listePos.toSet());
+  print("Partie 1 : Nombre d'arbres visibles --> ${listePos.toSet().length}");
 
-  print(listePos.toSet().length);
+
+  /* Partie 2 */
+  List<int> scoresPano = [];
+  for(int i = 0 ; i < nbLigne ; i++){
+    for(int j = 0; j < nbColonne ; j++){
+      print("[$i][$j]");
+      if(i == 0 || j == 0){
+        scoresPano.add(0);
+      }
+      else{
+        // Exploration a gauche
+        print("gauche");
+        int gauche = 0;
+        int max = carte[i][j];
+        loop_gauche:
+        for(int g = j-1; g >= 0 ; g--){
+          gauche ++;
+          if(carte[i][g] >= max){
+            break loop_gauche;
+          }
+        }
+
+        // Exploration a droite
+        print("droite");
+        int droite = 0;
+        max = carte[i][j];
+        loop_droite:
+        for(int d = j+1; d < nbColonne ; d++){
+          droite ++;
+          if(carte[i][d] >= max){
+            break loop_droite;
+          }
+        }
+
+        // Exploration a haut
+        print("haut");
+        int haut = 0;
+        max = carte[i][j];
+        loop_haut:
+        for(int h = i+1; h < nbLigne ; h++){
+          haut ++;
+          if(carte[h][j] >= max){
+            break loop_haut;
+          }
+        }
+
+        // Exploration a bas
+        print("bas");
+        int bas = 0;
+        max = carte[i][j];
+        loop_bas:
+        for(int b = i-1; b >= 0 ; b--){
+          bas++;
+          if(carte[b][j] >= max){
+            break loop_bas;
+          }
+        }
+
+        scoresPano.add(gauche * droite * haut * bas);
+      }
+    }
+  }
+  print("Partie 2 : Score Pano --> ${scoresPano.max}");
 
 }
