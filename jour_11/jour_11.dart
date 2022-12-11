aa507b8f71c4ce1403df4909489375c6ae6e459b
Rémi Decourt <fralacticus@gmail.com>
2022-12-11 17:34:47 +0100

    Update jour_11.dart

--- jour_11/jour_11.dart
+++ jour_11/jour_11.dart
@@ -44,9 +44,9 @@ class Singe{
   }
 
   bool tester(int objet){
-    //print("test : $objet");
     return _operationTest(objet);
   }
+
   void _creerOperationInspecte(String operateur, String valeur, String numPartie){
     int? integerValeur = int.tryParse(valeur);
     if(numPartie == "P1"){
@@ -88,27 +88,23 @@ void main(){
 
   List<List<String>> strSinges = lignes.slices(7).toList();
   for(List<String> s in strSinges){
-    //print("-- Singe --");
     RegExp regNombre = RegExp(r'([0-9]+|old)');
     RegExp regOperateur = RegExp(r'(\+|\*)');
     // Objets
     List<String?> strObjetsTemp = regNombre.allMatches(s[1]).map((z) => z.group(0)).toList();
     List<String> strObjets = strObjetsTemp.where((c) => c != null).cast<String>().toList();
-    //print(strObjets);
     // Operation
     String? strOperateurOperation = regOperateur.firstMatch(s[2])![0];
     String? strValeurOperation = regNombre.allMatches(s[2]).map((z) => z.group(0)).last;
-    //print("$strOperateurOperation ; $strValeurOperation");
     // Divisible
     String? strValeurDivisible =  regNombre.firstMatch(s[3])![0];
     Singe.moduloZinzin *= int.parse(strValeurDivisible!);
-    //print(strValeurDivisible);
     // Lancer
     String? strSingeTrue =  regNombre.firstMatch(s[4])![0];
     String? strSingeFalse =  regNombre.firstMatch(s[5])![0];
-    //print("$strSingeTrue ; $strSingeFalse");
 
-    singes.add(Singe.fromStrings(strObjets, strOperateurOperation!, strValeurOperation!, strValeurDivisible!, strSingeTrue!, strSingeFalse!, numPartie));
+
+    singes.add(Singe.fromStrings(strObjets, strOperateurOperation!, strValeurOperation!, strValeurDivisible, strSingeTrue!, strSingeFalse!, numPartie));
 
   }
 
@@ -128,9 +124,7 @@ void main(){
   }
 
   for(int t = 0; t < nbTours; t++){
-    print("Tour $t");
     for(int i = 0 ; i < singes.length ; i++){
-      print("--- Singe $i ---");
       singes[i].joueTour(singes);
     }
   }
