45cc1e9d702352e77206fa175248547512e09082
Rémi Delcourt <fralacticus@gmail.com>
2022-12-13 11:26:24 +0100

    jour 12 terminé

--- jour_12/jour_12.dart
+++ jour_12/jour_12.dart
@@ -1,199 +1,124 @@
-import 'dart:collection';
 import "dart:io";
-
+import "dart:math" ;
 import 'package:collection/collection.dart';
-import 'package:graphs/graphs.dart' as pgraph;
 
-class Case {
-  late int x, y;
-  late int val ;
-  Case.init({required this.x, required this.y, this.val = -1});
-  Case();
-  @override
-  String toString() => "$y,$x";
-  @override
-  bool operator ==(Object other) => other is Case && x == other.x && y == other.y;
+/* Repris de mykdavies */
+
+void main() {
+  List<String> lignes = File("./jour_12/input.txt").readAsLinesSync();
+  print("Partie 1 : ${part1(lignes)} noeuds visités");
+  print("Partie 2 : ${part2(lignes)} noeuds visités");
 }
 
-class Graph {
-  final Map<Node, List<Node>> nodes;
-
-  Graph(this.nodes);
-
-
+extension IntegerRangeExtension on int {
+  List<int> to(int end) => List.generate(end - this, (i) => i + this);
 }
 
 class Node {
-  late final String id;
-  final int data = 1;
-  late int x, y;
-  late int val ;
+  Point id;
+  int height;
+  Node(this.id, this.height);
+}
 
-  Node.init(this.id);
-  Node();
+class Edge {
+  Point from, to;
+  int weight;
+  Edge(this.from, this.to, this.weight);
+}
 
-  @override
-  bool operator ==(Object other) => other is Node && other.id == id;
+class Graph {
+  var nodes = <Point, Node>{};
+  var edges = <String, Edge>{};
+  var connected = <Point, Set<Point>>{};
+  Graph();
 
-  @override
-  int get hashCode => id.hashCode;
+  Node ensure(Node n) => nodes.putIfAbsent(n.id, () => n);
 
-  @override
-  String toString() => '<$id>';
+  void addEdge(Point from, Point to, int weight) {
+    edges["$from/$to"] = Edge(from, to, weight);
+    connected.putIfAbsent(from, () => {}).add(to);
+  }
+
+  List<Point> neighboursOf(Point node) => connected[node]!.toList();
+  Edge? edgeBetween(Point node1, Point node2) => edges["$node1/$node2"];
 }
 
 
-
-typedef Grille<T> = List<List<T>>;
-void main(){
-  List<String> lignes = File("./jour_12/exemple.txt").readAsLinesSync();
-
-  print("- Création grille de chaines");
-  Grille<String> strGrille = [];
-  strGrille.addAll(lignes.map((e) => e.split("")));
-
-  print("- Calcul largeur et hauteur de la grille");
-  int largeur = strGrille[0].length;
-  int hauteur = strGrille.length;
-
-  print("- Récupération coordonnées de la case de départ et d'arrivée");
-  Node coordDepart = trouverCoord(strGrille, largeur, hauteur, "S");
-  Node coordArrivee = trouverCoord(strGrille, largeur, hauteur, "E");
-  print("   Depart: $coordDepart   --  Arrivee: $coordArrivee");
-
-  print("- Substitution des valeurs des cases départ et arrivée par leurs lettres");
-  strGrille[coordDepart.y][coordDepart.x] = "a";
-  strGrille[coordArrivee.y][coordArrivee.x] = "z";
-
-  print("- Conversion grille de lettres en grille de Cases");
-  Grille<Node> grille = convertirEnGrilleCases(strGrille, largeur, hauteur);
-  print(grille);
-
-  print("- Création du graphe");
-  Graph graphe = Graph(voisins(grille, hauteur, largeur));
-  print(graphe.nodes);
-
-  print("- Dijkstra");
-
-  var dij = pgraph.shortestPath<Node>(coordDepart, coordArrivee, (node) => graphe.nodes[node] ?? [] );
-  print(dij);
-
-}
-
-
-Map<String, String> dijkstra(Map<String, List<String>> graph, String startNode, String endNode) {
-  // Set of nodes that have been visited
-  Set<String> visited = new Set<String>();
-
-  // Set of nodes that are candidates for being visited next
-  PriorityQueue<String> candidates = PriorityQueue<String>();
-
-  // Map of nodes to their previous node in the shortest path
-  Map<String, String> previousNodes = new Map<String, String>();
-
-  // Start by visiting the start node
-  visited.add(startNode);
-  candidates.add(startNode);
-
-  while (candidates.isNotEmpty) {
-    // Get the node with the shortest path from the start node
-    String currentNode = candidates.removeFirst();
-
-    // Check if we have reached the end node
-    if (currentNode == endNode) {
-      break;
-    }
-
-    // Visit all of the current node's neighbors
-    for (String neighbor in graph[currentNode]!) {
-      if (!visited.contains(neighbor)) {
-        visited.add(neighbor);
-        candidates.add(neighbor);
-        previousNodes[neighbor] = currentNode;
+var cameFrom = <Point, Point>{};
+Map<Point, int> dijkstra(Graph graph, Point start, Point end) {
+  cameFrom = {start: Point(-1, -1)};
+  var costSoFar = {start: 0};
+  var frontier = PriorityQueue<Point>(
+          (a, b) => (costSoFar[a]!).compareTo((costSoFar[b]!)));
+  frontier.add(start);
+  while (frontier.isNotEmpty) {
+    var current = frontier.removeFirst();
+    if (current == end) break;
+    for (var next in graph.neighboursOf(current)) {
+      var newCost =
+          costSoFar[current]! + graph.edgeBetween(current, next)!.weight;
+      if (!costSoFar.containsKey(next) || newCost < costSoFar[next]!) {
+        costSoFar[next] = newCost;
+        frontier.add(next);
+        cameFrom[next] = current;
       }
     }
   }
-
-  return previousNodes;
+  return costSoFar;
 }
 
-
-Grille<Node> convertirEnGrilleCases (Grille<String> grille, int largeur, int hauteur){
-  Grille<Node> caseGrille = List.generate(hauteur, (index) => List.generate(largeur, (index) => Node(), growable: false), growable: false);
-  for (int i = 0; i < hauteur; i++) {
-    for (int j = 0; j < largeur; j++) {
-      caseGrille[i][j].val = (grille[i][j]).codeUnitAt(0);
-      caseGrille[i][j].x = j;
-      caseGrille[i][j].y = i;
-      caseGrille[i][j].id = "$i,$j";
-
+late int width, height;
+Graph buildGraph(List<String> lines) {
+  var g = Graph();
+  width = lines.first.length;
+  height = lines.length;
+  var points = [
+    for (var y in 0.to(lines.length))
+      for (var x in 0.to(lines.first.length)) Point(x, y)
+  ];
+  for (var p in points) {
+    var c = lines[p.y].substring(p.x, p.x + 1);
+    if (c == 'S') {
+      start = p;
+      c = 'a';
+    } else if (c == 'E') {
+      end = p;
+      c = 'z';
     }
+    var h = c.codeUnitAt(0) - 'a'.codeUnitAt(0);
+    g.ensure(Node(p, h));
   }
-  return caseGrille;
+  return g;
 }
 
-Node trouverCoord(Grille<String> grille, int largeur, int hauteur, String element){
-  for (int i = 0; i < hauteur; i++) {
-    for (int j = 0; j < largeur; j++) {
-        if(grille[i][j] == element){
-          Node node = Node();
-          if(element == "S"){
-            node.val = "a".codeUnitAt(0);
-          }
-          else if(element == "E"){
-            node.val = "z".codeUnitAt(0);
-          }
-          node.x = j;
-          node.y = i;
-          node.id = "$i,$j";
-          return node;
-        }
-    }
+typedef HeightTest = bool Function(int x, int y);
+var dirs = [Point(0, 1), Point(0, -1), Point(1, 0), Point(-1, 0)];
+
+void buildEdges(Graph g, HeightTest edgeTest) {
+  for (var h in g.nodes.keys) {
+    var hh = g.nodes[h]!.height;
+    dirs
+        .map((d) => h + d)
+        .where((n) => g.nodes.containsKey(n))
+        .where((n) => edgeTest(g.nodes[n]!.height, hh))
+        .forEach((n) => g.addEdge(h, n, 1));
   }
-  throw "Erreur";
 }
 
+late Point start, end;
+part1(List<String> lines) {
+  Graph g = buildGraph(lines);
+  buildEdges(g, (hh, hn) => hh - hn <= 1);
+  Map<Point, int> path = dijkstra(g, start, end);
+  return path[end];
+}
 
-Map<Node, List<Node>>  voisins(Grille<Node> grille, int hauteur, int largeur) {
-
-  Map<Node, List<Node>> graphe = {};
-  for (int i = 0; i < hauteur; i++) {
-    for (int j = 0; j < largeur; j++) {
-      List<Node> voisins = [];
-      Node caseVoisine;
-      Node caseActuelle = grille[i][j];
-
-      // Voisin en haut
-      if(i > 0 ){
-        caseVoisine = grille[i-1][j];
-        if([caseActuelle.val-1, caseActuelle.val, caseActuelle.val+1].contains(caseVoisine.val) ){
-          voisins.add(caseVoisine);
-        }
-      }
-      // Voisin en bas
-      if(i < hauteur-1){
-        caseVoisine = grille[i+1][j];
-        if([caseActuelle.val-1, caseActuelle.val, caseActuelle.val+1].contains(caseVoisine.val) ){
-          voisins.add(caseVoisine);
-        }
-      }
-      // Voisin à gauche
-      if( j > 0 ){
-        caseVoisine = grille[i][j-1];
-        if([caseActuelle.val-1, caseActuelle.val, caseActuelle.val+1].contains(caseVoisine.val) ){
-          voisins.add(caseVoisine);
-        }
-      }
-      // Voisin à droite
-      if(j < hauteur-1){
-        caseVoisine = grille[i][j+1];
-        if([caseActuelle.val-1, caseActuelle.val, caseActuelle.val+1].contains(caseVoisine.val) ){
-          voisins.add(caseVoisine);
-        }
-      }
-
-      graphe[caseActuelle] = voisins;
-    }
-  }
-  return graphe;
+part2(List<String> lines) {
+  Graph g = buildGraph(lines);
+  buildEdges(g, (hh, hn) => hn - hh <= 1);
+  Map<Point, int> path = dijkstra(g, end, start);
+  return path.entries
+      .where((e) => g.nodes[e.key]!.height == 0)
+      .map((e) => e.value)
+      .min;
 }
